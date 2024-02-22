17451411f7dccf47ac709c88c659e9f05f63210e
niklas eriksson <niklaseriksson32@hotmail.com>
2024-02-22 11:59:03 +0000

    changed define ship function

--- run.py
+++ run.py
@@ -9,22 +9,18 @@ def print_board(board):
     for row in board:
         print((" ").join(row))
 
-
-def battle_ships(board):
-    for ship in range(3):
-        ship_row, ship_col = randint(0,5), randint(0,5)
-        while board[ship_row][ship_col] == "X":
-            ship_row, ship_col = randint(0,5), randint(0,5)
-        board[ship_row][ship_col] = "X"
+def random_ship(board):
+    ship_row = randint(0, len(board) - 1)
+    ship_col = randint(0, len(board) - 1)
+    return ship_row, ship_col
 
 
-def find_ships():
-    row = input("Guess a row between 1-5: ")
-    col = input("guess a column between 1-5: ")
-    while row or col not in "12345":
-        print("you must guess a number between 1-5")
-        row = input("Guess a row between 1-5: ")
-        col = input("guess a column between 1-5: ")
-    
-    return int(row), int(col)
+def guess_ship(board):
+    guess_row = int(input("guess a row between 1-5: "))
+    guess_col = int(input("gess a column between 1-5: "))
+
+    if guess_row == ship_row and guess_col == ship_col:
+        print("Congrats you have sunk my battleship!")
+
+        
 
